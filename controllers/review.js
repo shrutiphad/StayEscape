2b37da24808021f100ec5f34c7b0adde180d0c78
shrutiphad <shrutiphadwork@gmail.com>
2025-09-01 18:29:12 +0530

    Commenst

--- controllers/review.js
+++ controllers/review.js
@@ -2,19 +2,6 @@ const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
 
 
-// module.exports.createReview=async(req,res) =>{
-//  let listing =await Listing.findById(req.params.id);
-//  let newReview = new Review(req.body.review);
-//  newReview.author = req.user._id;
-//  listing.reviews.push(newReview);
-
-//  await newReview.save();
-//  await listing.save();
-
-// req.flash("success","New Review Created!");
-// res.redirect(`/listings/${listing._id}`);
-// };
-
 
 module.exports.createReview = async (req, res) => {
   console.log(" HIT createReview");
@@ -23,7 +10,7 @@ module.exports.createReview = async (req, res) => {
 
   const listing = await Listing.findById(req.params.id);
   if (!listing) {
-    console.log("❌ Listing not found");
+    console.log("Listing not found");
     req.flash("error", "Listing not found");
     return res.redirect("/listings");
   }
@@ -45,7 +32,7 @@ module.exports.deleteReview = async(req,res) =>{
     let { id, reviewId } =req.params;
   
     await Listing.findByIdAndUpdate( id,{$pull:{reviews : reviewId}});
-    await Review.findByIdAndDelete(reviewId); // ✅ Correct
+    await Review.findByIdAndDelete(reviewId); 
   
           req.flash("error","Your Review is Deleted!");
           res.redirect(`/listings/${id}`);
