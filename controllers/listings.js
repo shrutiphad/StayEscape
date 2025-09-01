73e9902322a18be68f61102c678a7327a54cedaa
shrutiphad <shrutiphadwork@gmail.com>
2025-09-01 19:13:57 +0530

    Remove duplicate commented-out createListing function

--- controllers/listings.js
+++ controllers/listings.js
@@ -52,29 +52,6 @@ module.exports.createListing = async (req,res,next) => {
     };
 
 
-//     module.exports.createListing = async (req, res, next) => {
-//     try {
-//         if (!req.file) {
-//             req.flash("error", "Image upload failed. Please try again.");
-//             return res.redirect("/listings/new");
-//         }
-
-//         const { path: url, filename } = req.file; // ✅ destructure safely
-//         const newListing = new Listing(req.body.listing);
-
-//         newListing.owner = req.user._id;
-//         newListing.image = { url, filename };
-
-//         await newListing.save();
-
-//         req.flash("success", "New Listing Created!");
-//         res.redirect("/listings");
-//     } catch (err) {
-//         next(err);
-//     }
-// };
-
-
 
 
 
